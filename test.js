a4b07b28f5538c1fae814124df8cbad908b49c77
joshua142857 <joshua142857@gmail.com>
2025-02-11 16:42:51 -0600

    test2

---
+++ test.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const scene = document.createElement("div");
+    scene.classList.add("scene");
+
+    const cube = document.createElement("div");
+    cube.classList.add("cube");
+
+    for (let i = 0; i < 6; i++) {
+        const face = document.createElement("div");
+        face.classList.add("face", `face${i}`);
+        cube.appendChild(face);
+    }
+
+    scene.appendChild(cube);
+    document.body.appendChild(scene);
+
+    let angleX = 0, angleY = 0;
+
+    function rotateCube() {
+        angleX += 1;
+        angleY += 1;
+        cube.style.transform = `rotateX(${angleX}deg) rotateY(${angleY}deg)`;
+        requestAnimationFrame(rotateCube);
+    }
+
+    rotateCube();
+});
